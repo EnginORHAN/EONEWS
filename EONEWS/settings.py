e74eaab2ae10431c71f441fc931ec616ab382070
Engin <enginorhan2000@gmail.com>
2024-02-09 14:56:17 +0300

    Debug False

--- EONEWS/settings.py
+++ EONEWS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*q8r4($n#j*t0y^8t$dzri(gwiu6+uet%%vq61(g(tvn*0a(ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
